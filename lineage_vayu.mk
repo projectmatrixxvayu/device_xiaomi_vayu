314d8a70fd81aac1c9622646dd4561cb4550febe
Mo'men Abdulrazek Ali <100705049+momenabdulrazek@users.noreply.github.com>
2024-07-24 06:05:07 +0300

    rising os build time !

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -13,41 +13,29 @@ $(call inherit-product, device/xiaomi/vayu/device.mk)
 # Inherit common AOSP configurations
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Matrixx
-MATRIXX_BUILD_TYPE := Unofficial
-MATRIXX_MAINTAINER := momenabdulrazekali
-MATRIXX_CHIPSET := SM8150
-MATRIXX_BATTERY := 5160mah
-MATRIXX_DISPLAY := 1080x2400
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
 
-# Device identifier
 PRODUCT_BRAND := POCO
 PRODUCT_DEVICE := vayu
 PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := POCO X3 Pro
 PRODUCT_NAME := lineage_vayu
-
 PRODUCT_SYSTEM_NAME := vayu_global
-PRODUCT_SYSTEM_DEVICE := vayu
 
-# GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-TARGET_GAPPS_ARCH := arm64
-
-#Bootanim
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# Gapps
-WITH_GMS := true
-BUILD_GOOGLE_CONTACTS := true
-BUILD_GOOGLE_DIALER := true
-BUILD_GOOGLE_MESSAGE := true
-
-# Exclude Redundants 
-TARGET_EXCLUDES_AUDIOFX := true
-TARGET_EXCLUDES_VIA := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vayu_global-user 13 TKQ1.221013.002 V14.0.3.0.TJUMIXM release-keys" \
-    PRODUCT_MODEL=M2102J20SG
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME) \
+    RISING_MAINTAINER="momenabdulrazekali" \
+    RISING_CHIPSET="Snapdragon860"
 
 BUILD_FINGERPRINT := POCO/vayu_global/vayu:13/RKQ1.200826.002/V14.0.3.0.TJUMIXM:user/release-keys
+
+#RisingOS
+TARGET_ENABLE_BLUR := true
+WITH_GMS := true
+RISING_MAINTAINER := momenabdulrazekali
+RISING_BUILDTYPE := UNOFFICIAL
